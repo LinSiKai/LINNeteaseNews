c91b822901d271985d8051ff2e43cfb1bca148a4
admin <admin@admindemacbook-pro.local>
2016-04-12 23:16:28 +0800

    初始化项目

---
+++ NetEaseNews/NetEaseNews/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  NetEaseNews
+//
+//  Created by admin on 16/4/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
